e3a4471ccf04bcdc642ccf8a04b2ffb674fb7f1c
Sam Willis <sam.willis@event.ly>
2013-08-09 19:00:18 +0100

    fixing homepage slider
    adding images and removing nav with js

---
+++ _site/js/slider.js
@@ -0,0 +1,12 @@
+// Can also be used with $(document).ready()
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    itemWidth: 210,
+    itemMargin: -10,
+    minItems: 2,
+    maxItems: 2,
+    controlNav: false,
+    directionNav: false
+  });
+});
