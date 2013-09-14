5d0e6017121b7356e78d86a7b44b5122a0af9abb
Sam Willis <sam.willis@event.ly>
2013-09-14 22:31:34 +0100

    adding final photos

--- _site/js/slider.js
+++ _site/js/slider.js
@@ -2,9 +2,10 @@
 $(window).load(function() {
   $('.flexslider').flexslider({
     animation: "slide",
-    itemWidth: 450,
+    itemWidth: 500,
     itemMargin: 10,
     controlNav: false,
-    directionNav: false
+    directionNav: false,
+    move: 1
   });
 });
