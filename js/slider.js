1cd1bd24e96ce0f27b0bb944c6f366c3821de029
Sam Willis <sam.willis@event.ly>
2013-11-12 17:37:41 +0000

    Fix typo and other errors

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,23 @@
 // Can also be used with $(document).ready()
 $(window).load(function() {
 
+  var sum = 0,
+      count = -1,
+      all = $('ul.slides > li');
+  all.each(function() {
+      sum += +$('img:eq(0)', this).width();
+      count++;
+  });
+
+  var avWidth = sum / count;
+
   $('.flexslider').flexslider({
     animation: "slide",
-    itemWidth: 750,
+    itemWidth: avWidth,
     itemMargin: 10,
     controlNav: false,
     directionNav: true,
-    move: 2,
+    // move: 2,
     slideshow: false
   });
 });
