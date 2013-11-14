9bd704c6c62ca3c4a83b6c30643156ecc9f2c1ad
Sam Willis <sam.willis@event.ly>
2013-11-14 17:31:54 +0000

    Fix load issues with slider

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(window).load(function() {
     if($(".flexslider.banners").length > 0) {
         $('.flexslider.banners').flexslider({
             animation: "slide",
@@ -6,17 +6,11 @@ $(document).ready(function() {
             controlNav: false,
             slideshow: false,
             smoothHeight: true,
-            itemMargin: 100,
-            variableImageWidth: true,
-            move: 0,
-            before: function(slider) {
-          var player = slider.slides.eq(slider.currentSlide).find('iframe.youtube');
-
-        if (player.length !== 0) {
-          var func = 'pauseVideo'; //this === frame ? 'playVideo' : 'pauseVideo'
-          player[0].contentWindow.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
-        }
-      },
+            variableImageWidth: true
         });
     }
 });
+
+$(window).load(function() {
+  $('.flexslider.banners').fadeIn(1000);
+})
